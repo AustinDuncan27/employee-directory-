e837167febb5bb6d34d68553234ca2267af89dcf
Austin Duncan <your_email@example.com>
2026-01-15 14:17:36 -0600

    done

---
+++ app.js
@@ -0,0 +1,30 @@
+const express= require('express');
+const app= express();
+
+const employees =require('./db/employees');
+app.get('/', (req,res) => {
+  res.send('Hello Employees!');
+
+});
+
+app.get('/employees', (req,res) => {
+  res.json(employees);
+});
+
+app.get ('/employees/random', (req,res) => {
+  const index= Math.floor(Math.random() * employees.length);
+  res.json(employees[index]);
+});
+
+app.get ('/employees/:id', (req,res) => {
+  const id= Number(req.params.id);
+  const employee = employees.find(emp => emp.id ===id);
+
+  if (!employee) {
+    return res.status(404).send('Employee not found');
+  }
+
+  res.json(employee);
+})
+
+module.exports= app;
